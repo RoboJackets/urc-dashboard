a76cdfd5ef2266cc9e1062dbabf18ff753f94b64
Abhiram Ghanta <abhiramg2021@gmail.com>
2023-10-30 22:22:17 -0400

    updated websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import { ControlPanel } from "./components/ControlPanel/ControlPanel";
 import ROSLIB from "roslib";
 import { Navigation } from "./components/Navigation/Navigation";
+import { useState } from "react";
 function App() {
-  const ROS = new ROSLIB.Ros({ url: "ws://localhost:9090" });
+  let ROS: any;
+
+  const [link, setLink] = useState<string>("ws://localhost:9090");
+  const [linkReceived, setLinkReceived] = useState(false);
+
+  if (!linkReceived) {
+    return (
+      <div className="App w-screen h-screen p-2 flex gap-2 flex-col justify-center items-center">
+        <div className="card">
+          <div className="card-title">Input Rosbridge URL</div>
+          <input
+            placeholder="ws://localhost:9090"
+            className="w-[200px] h-min"
+            onChange={(e) => setLink(e.target.value)}
+          />
+          <button
+            className="text-white py-2 px-4 rounded bg-yellow-500"
+            onClick={() => {
+              if (link) {
+                setLinkReceived(true);
+              }
+            }}
+          >
+            Set Rosbridge URL
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    ROS = new ROSLIB.Ros({ url: link });
+  }
+
   return (
     <div className="App w-screen h-screen p-2 flex gap-2">
       <ControlPanel ROS={ROS} />
