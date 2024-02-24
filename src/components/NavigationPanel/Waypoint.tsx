95290a4239ccdc22221bf57efa8f0b1641139e1f
Mrinal Jain <mjain330@gatech.edu>
2024-02-23 19:11:02 -0500

    waypoint ROS messages

--- src/components/NavigationPanel/Waypoint.tsx
+++ src/components/NavigationPanel/Waypoint.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { Coordinate } from "./CoordinateInterface";
+import ROSLIB from "roslib";
 
 interface WaypointProps {
   waypoint: Coordinate;
@@ -12,11 +14,42 @@ interface WaypointProps {
 export const Waypoint = (props: WaypointProps) => {
   const [lat, setLat] = useState<number>(props.waypoint.lat);
   const [lng, setLng] = useState<number>(props.waypoint.lng);
+  const [statusMessage, setStatusMessage] = useState<string>("");
+
+  const WaypointTopic = new ROSLIB.Topic({
+    ros: props.ROS,
+    name: "/waypoint",
+    messageType: "urc_msgs/Waypoint",
+  });
+
+  const WaypointStatusTopic = new ROSLIB.Topic({
+    ros: props.ROS,
+    name: "/current_navigation_state",
+    messageType: "urc_msgs/NavigationStatus",
+  });
+
+  useEffect(() => {
+    WaypointTopic.subscribe((message: any) => {
+      props.setWaypoint({
+        id: 1,
+        lat: message.latitude,
+        lng: message.longitude,
+      })
+      props.setWaypointActive(true);
+    });
+    
+    WaypointStatusTopic.subscribe((message: any) => {
+      setStatusMessage(message.message);
+      if (message.message === "NoWaypoint") {
+        props.setWaypointActive(false);
+      }
+    });
+  });
 
   return (
     <div className="p-4 card">
       <div className="card-subtitle">Waypoint</div>
-      <div className="whitespace-nowrap">{`Status: ${props.waypointActive ? "Waypoint Set" : "No Waypoint Set"}`}</div>
+      <div className="whitespace-nowrap">{`Status: ${statusMessage}`}</div>
       <div className="whitespace-nowrap">{`Lat: ${props.waypoint.lat}, Lng: ${props.waypoint.lng}`}</div>
 
       <div className="flex gap-2">
@@ -39,8 +72,13 @@ export const Waypoint = (props: WaypointProps) => {
           lat: lat,
           lng: lng
         });
+        WaypointTopic.publish(new ROSLIB.Message({
+          latitude: lat,
+          longitude: lng,
+        }));
         props.setWaypointActive(true);
       }}>Set Waypoint</button>
+
       <button onClick={() => {
         props.setWaypointActive(false);
       }}>Remove Waypoint</button>
