c5275db6027bb597ce415a5be0722b3240ff0b64
Mrinal Jain <mjain330@gatech.edu>
2024-02-28 14:48:58 -0500

    publish current coordinates as base

--- src/components/NavigationPanel/GpsCoords.tsx
+++ src/components/NavigationPanel/GpsCoords.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ROSLIB from "roslib";
 import { Coordinate } from "./CoordinateInterface";
 interface GpsProps {
@@ -8,10 +8,18 @@ interface GpsProps {
 }
 
 export const GPS = (props: GpsProps) => {
+  const [baseCoord, setBaseCoord] = useState<boolean>(false);
+
   const gpsTopic = new ROSLIB.Topic({
     ros: props.ROS,
     name: "/fix",
     messageType: "sensor_msgs/NavSatFix",
+  });
+
+  const baseTopic = new ROSLIB.Topic({
+    ros: props.ROS,
+    name: "/set_base",
+    messageType: "std_msgs/Bool",
   });
 
   useEffect(() => {
@@ -26,6 +34,15 @@ export const GPS = (props: GpsProps) => {
     <div className="card">
       <div className="card-subtitle">GPS Coordinates</div>
       <div className="whitespace-nowrap">{`Lat: ${props.coord.lat.toPrecision(5)},  Lng: ${props.coord.lng.toPrecision(5)}`}</div>
+      <div className="whitespace-nowrap">{`Writing Base Coords: ${baseCoord ? "True" : "False"}`}</div>
+      <button onClick={() => {
+        baseTopic.publish(new ROSLIB.Message({
+          data: !baseCoord
+        }));
+        setBaseCoord(!baseCoord);
+      }}>
+        Toggle Base Coordinate Update
+      </button>
     </div>
   );
 };
