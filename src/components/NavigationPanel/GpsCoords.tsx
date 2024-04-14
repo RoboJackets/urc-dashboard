4670712f96b3fb5f49152bbb48607bec943a6ef2
Mrinal Jain <2mrinaljain@gmail.com>
2024-04-14 18:13:48 -0400

    base_fix topic for base station coords

--- src/components/NavigationPanel/GpsCoords.tsx
+++ src/components/NavigationPanel/GpsCoords.tsx
@@ -11,7 +11,6 @@ interface GpsProps {
 }
 
 export const GPS = (props: GpsProps) => {
-
   const gpsTopic = new ROSLIB.Topic({
     ros: props.ROS,
     name: "/fix",
@@ -20,8 +19,8 @@ export const GPS = (props: GpsProps) => {
 
   const baseTopic = new ROSLIB.Topic({
     ros: props.ROS,
-    name: "/set_base",
-    messageType: "std_msgs/Bool",
+    name: "/base_fix",
+    messageType: "sensor_msgs/NavSatFix",
   });
 
   useEffect(() => {
@@ -29,31 +28,37 @@ export const GPS = (props: GpsProps) => {
       props.setCoord({
         id: "R",
         lat: message.latitude,
-        lng: message.longitude
+        lng: message.longitude,
       });
     });
 
     baseTopic.subscribe((message: any) => {
-      if (message.data) {
-        props.setBaseCoord({
-          id: "B",
-          lat: props.coord.lat,
-          lng: props.coord.lng
-        });
-      }
+      props.setBaseCoord({
+        id: "B",
+        lat: message.latitude,
+        lng: message.longitude,
+      });
     });
   });
   return (
     <div className="card">
       <div className="card-subtitle">GPS Coordinates</div>
       <div className="whitespace-nowrap">{`Lat: ${props.coord.lat.toPrecision(9)},  Lng: ${props.coord.lng.toPrecision(9)}`}</div>
-      <button onClick={() => {
-        props.setBaseCoord({id: "B", lat: props.coord.lat, lng: props.coord.lng});
-        baseTopic.publish(new ROSLIB.Message({
-          data: true
-        }));
-        console.log(props.coord);
-      }}>
+      <button
+        onClick={() => {
+          props.setBaseCoord({
+            id: "B",
+            lat: props.coord.lat,
+            lng: props.coord.lng,
+          });
+          baseTopic.publish(
+            new ROSLIB.Message({
+              data: true,
+            }),
+          );
+          console.log(props.coord);
+        }}
+      >
         Set Base Station
       </button>
     </div>
