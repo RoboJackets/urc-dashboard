e85480b84b778b6946be53e7bdc01bd0c9edd56a
katya <ekaterina.tumanova@gmail.com>
2024-11-24 18:54:27 -0500

    finalized status panel with connections to ROS topics

--- src/components/StatusPanel/Statuses.tsx
+++ src/components/StatusPanel/Statuses.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
-import { Status } from "./Status";
 import ROSLIB from "roslib";
 import { HeartbeatSubscriber } from "./HeartbeatSubscriber";
+import { ControllerSubscriber } from "./ControllerSubscriber";
+import { Chip, Stack }  from '@mui/material';
 
 interface StatusesProps {
   ROS: ROSLIB.Ros;
+  isDark: boolean;
 }
 
 export enum StatusColors {
-  RED = "bg-red-400 dark:bg-red-500",
-  GREEN = "bg-green-400 dark:bg-green-500",
+  RED = "error",
+  GREEN = "success",
 }
 
 export const Statuses = (props: StatusesProps) => {
   const [bridgeStatus, setBridgeStatus] = useState<StatusColors>(StatusColors.RED);
   const [robotStatus, setRobotStatus] = useState<StatusColors>(StatusColors.RED);
+  const [controllerStatus, setControllerStatus] = useState<StatusColors>(StatusColors.RED);
 
   // Rosbridge Status
   props.ROS.on("connection", () => setBridgeStatus(StatusColors.GREEN));
@@ -23,12 +26,20 @@ export const Statuses = (props: StatusesProps) => {
   });
   props.ROS.on("close", () => setBridgeStatus(StatusColors.RED));
 
+  // shows the rosbridge chip (was already updated with ROS earlier)
+  // updates the robot status with the heartbeat subscriber + shows the robot chip
+  // updates the controller status with the controller subscriber + shows the controller chip
   return (
-    <div className="card">
-      <div className="card-subtitle">Status</div>
-      <Status value="Rosbridge" color={bridgeStatus} />
-      <Status value="Robot" color={robotStatus} />
-      <HeartbeatSubscriber ROS={props.ROS} setRobotStatus={setRobotStatus} />
-    </div>
+    <Stack spacing={1} sx={{ alignItems: 'center' }}>
+      <Stack direction="row" spacing={1}>
+        <Chip label="Rosbridge" color={bridgeStatus} />
+
+        <HeartbeatSubscriber ROS={props.ROS} setRobotStatus={setRobotStatus} />
+        <Chip label="Robot" color={robotStatus} />
+        
+        <ControllerSubscriber ROS={props.ROS} setStatus={setControllerStatus} />
+        <Chip label="Controller" color={controllerStatus} />
+      </Stack>
+    </Stack>
   );
 };
