6d67bbd5c8808e8d7caf00f5fbcef0b20b70b92d
shayaf84 <shayafmd@gmail.com>
2025-02-23 18:59:00 -0500

    Method to invoke service when waypoint is added

--- src/WaypointsManager.tsx
+++ src/WaypointsManager.tsx
@@ -1,3 +1,4 @@
+import ROSLIB
 import React, { useState } from "react";
 import {
   TextField,
@@ -36,11 +37,38 @@ export const WaypointsManager: React.FC = () => {
       };
       setWaypoints([...waypoints, newWaypoint]);
     }
+
+    const addWaypointService = new ROSLIB.Service({
+      //ros: ros,
+      //name: '/add_waypoint',
+      //serviceType: 'package/AddWaypoint',
+    });
+    
+    const request = new ROSLIB.ServiceRequest({
+      latitude = lastWaypoint.latitude,
+      longitude = lastWaypoint.longitude
+    })
+
+    addWaypointService.callService(request, (result: any) => {
+      console.log("Waypoint added: " + result);
+    })
+    
   };
 
   // Remove a waypoint by ID
   const removeWaypoint = (id: number): void => {
     setWaypoints(waypoints.filter((waypoint) => waypoint.id !== id));
+
+    const removeWaypointService = new ROSLIB.Service({
+      //ros: ros,
+      //name: '/remove_waypoint',
+      //serviceType: 'package/RemoveWaypoint'
+    });
+
+    const request = new ROSLIB.ServiceRequest({
+      id: id
+    })
+
   };
 
   // Handle input changes for latitude and longitude
