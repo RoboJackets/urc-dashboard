13e439bdfa7f20ec56992635015fa993c98d9c8f
Mrinal Jain <2mrinaljain@gmail.com>
2025-02-23 19:31:31 -0500

    fix duplicate messages

--- src/components/InfoPanel/ConsoleOut.tsx
+++ src/components/InfoPanel/ConsoleOut.tsx
@@ -22,6 +22,14 @@ interface ConsoleOutProps {
   isDark: boolean;
 }
 
+const levelToLevelName: { [key: number]: string } = {
+  10: "DEBUG",
+  20: "INFO",
+  30: "WARN",
+  40: "ERROR",
+  50: "FATAL",
+};
+
 export const ConsoleOut = (props: ConsoleOutProps) => {
   const [lowPriorityMsgs, setLowPriorityMsgs] = useState<any[]>([]);
   const [highPriorityMsgs, setHighPriorityMsgs] = useState<any[]>([]);
@@ -39,11 +47,29 @@ export const ConsoleOut = (props: ConsoleOutProps) => {
   useEffect(() => {
     ConsoleOutTopic.subscribe((message: any) => {
       console.log(message);
+
+      // remove duplicates
+      if (
+        lowPriorityMsgs.length > 0 &&
+        message.stamp.sec === lowPriorityMsgs[0].stamp.sec &&
+        message.stamp.nanosec === lowPriorityMsgs[0].stamp.nanosec
+      ) {
+        console.log("HERE");
+        return;
+      }
+      if (
+        highPriorityMsgs.length > 0 &&
+        message.stamp.sec === highPriorityMsgs[0].stamp.sec &&
+        message.stamp.nanosec === highPriorityMsgs[0].stamp.nanosec
+      ) {
+        return;
+      }
+
       if (message.level <= 20) {
         if (lowPriorityMsgs.length >= MAX_MSGS) {
-          setLowPriorityMsgs((msgs) => msgs.slice(1));
+          setLowPriorityMsgs((msgs) => msgs.slice(0, MAX_MSGS - 1));
         }
-        setLowPriorityMsgs((msgs) => [...msgs, message]);
+        setLowPriorityMsgs((msgs) => [message, ...msgs]);
       } else {
         setHighPriorityMsgs((msgs) => [message, ...msgs]);
       }
@@ -109,16 +135,20 @@ export const ConsoleOut = (props: ConsoleOutProps) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {displayedLogs.map((log, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{log.level}</TableCell>
-                    <TableCell>{log.name}</TableCell>
-                    <TableCell>{log.msg}</TableCell>
-                    <TableCell>
-                      {new Date(log.timestamp).toLocaleString()}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {displayedLogs
+                  .filter((_, index) => index % 2 === 0)
+                  .map((log, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{levelToLevelName[log.level]}</TableCell>
+                      <TableCell>{log.name}</TableCell>
+                      <TableCell>{log.msg}</TableCell>
+                      <TableCell>
+                        {new Date(
+                          log.stamp.sec * 1000 + log.stamp.nanosec,
+                        ).toLocaleString()}
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </TableContainer>
